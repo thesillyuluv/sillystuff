4114f9f0c6cf5d1dd65a6151c680f7478085c7b4
Silly <jdhdhelp@gmail.com>
2026-09-20 19:01:24 -0400

    Refactor onLoad and onUnload to default export

--- plugins/clydeeditor/src/index.ts
+++ plugins/clydeeditor/src/index.ts
@@ -4,33 +4,33 @@ import { storage } from "@vendetta/plugin";
 import Settings from "./Settings";
 
 const CLYDE_ID = "1";
-
 const UserStore = findByStoreName("UserStore");
 const AvatarUtils = findByProps("getUserAvatarSource");
-
 const patches: (() => void)[] = [];
 
-export const onLoad = () => {
-  storage.name ??= "";
-  storage.avatar ??= "";
-
-  patches.push(
-    after("getUser", UserStore, ([id]: string[], user: any) => {
-      if (id !== CLYDE_ID || !user || !storage.name) return;
-      user.username = storage.name;
-      user.globalName = storage.name;
-    })
-  );
-
-  if (AvatarUtils) {
-    patches.push(
-      after("getUserAvatarSource", AvatarUtils, ([user]: any[], res: any) => {
-        if (user?.id === CLYDE_ID && storage.avatar) return { uri: storage.avatar };
-      })
-    );
-  }
+export default {
+  onLoad() {
+    storage.name ??= "";
+    storage.avatar ??= "";
+    if (UserStore) {
+      patches.push(
+        after("getUser", UserStore, ([id]: string[], user: any) => {
+          if (id !== CLYDE_ID || !user || !storage.name) return;
+          user.username = storage.name;
+          user.globalName = storage.name;
+        })
+      );
+    }
+    if (AvatarUtils) {
+      patches.push(
+        after("getUserAvatarSource", AvatarUtils, ([user]: any[], res: any) => {
+          if (user?.id === CLYDE_ID && storage.avatar) return { uri: storage.avatar };
+        })
+      );
+    }
+  },
+  onUnload() {
+    patches.forEach((p) => p());
+  },
+  settings: Settings,
 };
-
-export const onUnload = () => patches.forEach((p) => p());
-
-export const settings = Settings;
