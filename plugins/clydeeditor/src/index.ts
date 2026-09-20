9d5f4ad9182be5d8f4f2fbc84e633f5c73c91159
Silly <jdhdhelp@gmail.com>
2026-09-20 18:40:33 -0400

    Implement Clyde editor plugin functionality

---
+++ plugins/clydeeditor/src/index.ts
@@ -0,0 +1,36 @@
+import { findByStoreName, findByProps } from "@vendetta/metro";
+import { after } from "@vendetta/patcher";
+import { storage } from "@vendetta/plugin";
+import Settings from "./Settings";
+
+const CLYDE_ID = "1";
+
+const UserStore = findByStoreName("UserStore");
+const AvatarUtils = findByProps("getUserAvatarSource");
+
+const patches: (() => void)[] = [];
+
+export const onLoad = () => {
+  storage.name ??= "";
+  storage.avatar ??= "";
+
+  patches.push(
+    after("getUser", UserStore, ([id]: string[], user: any) => {
+      if (id !== CLYDE_ID || !user || !storage.name) return;
+      user.username = storage.name;
+      user.globalName = storage.name;
+    })
+  );
+
+  if (AvatarUtils) {
+    patches.push(
+      after("getUserAvatarSource", AvatarUtils, ([user]: any[], res: any) => {
+        if (user?.id === CLYDE_ID && storage.avatar) return { uri: storage.avatar };
+      })
+    );
+  }
+};
+
+export const onUnload = () => patches.forEach((p) => p());
+
+export const settings = Settings;
