1ccc4cdd6c2e9e3b04bca737fd071ecc8c1a34aa
Silly <jdhdhelp@gmail.com>
2026-09-20 19:05:57 -0400

    Refactor onLoad method and add error handling

--- plugins/clydeeditor/src/index.ts
+++ plugins/clydeeditor/src/index.ts
@@ -1,7 +1,10 @@
 import { findByStoreName, findByProps } from "@vendetta/metro";
 import { after } from "@vendetta/patcher";
 import { storage } from "@vendetta/plugin";
+import { showToast } from "@vendetta/ui/toasts";
 import Settings from "./Settings";
+
+console.log("[ClydeEditor] index evaluated", { settings: typeof Settings });
 
 const CLYDE_ID = "1";
 const UserStore = findByStoreName("UserStore");
@@ -10,26 +13,45 @@ const patches: (() => void)[] = [];
 
 export default {
   onLoad() {
-    storage.name ??= "";
-    storage.avatar ??= "";
-    if (UserStore) {
-      patches.push(
-        after("getUser", UserStore, ([id]: string[], user: any) => {
-          if (id !== CLYDE_ID || !user || !storage.name) return;
-          user.username = storage.name;
-          user.globalName = storage.name;
-        })
-      );
-    }
-    if (AvatarUtils) {
-      patches.push(
-        after("getUserAvatarSource", AvatarUtils, ([user]: any[], res: any) => {
-          if (user?.id === CLYDE_ID && storage.avatar) return { uri: storage.avatar };
-        })
-      );
+    try {
+      console.log("[ClydeEditor] onLoad", {
+        UserStore: !!UserStore,
+        AvatarUtils: !!AvatarUtils,
+        name: storage.name,
+        avatar: storage.avatar,
+      });
+      showToast("Clyde Editor loaded");
+
+      storage.name ??= "";
+      storage.avatar ??= "";
+
+      if (UserStore) {
+        patches.push(
+          after("getUser", UserStore, ([id]: string[], user: any) => {
+            if (id !== CLYDE_ID || !user || !storage.name) return;
+            console.log("[ClydeEditor] renaming Clyde to", storage.name);
+            user.username = storage.name;
+            user.globalName = storage.name;
+          })
+        );
+      }
+      if (AvatarUtils) {
+        patches.push(
+          after("getUserAvatarSource", AvatarUtils, ([user]: any[], res: any) => {
+            if (user?.id === CLYDE_ID && storage.avatar) {
+              console.log("[ClydeEditor] swapping Clyde avatar");
+              return { uri: storage.avatar };
+            }
+          })
+        );
+      }
+    } catch (e) {
+      console.log("[ClydeEditor] onLoad error", e);
+      showToast("Clyde Editor error, check logs");
     }
   },
   onUnload() {
+    console.log("[ClydeEditor] onUnload");
     patches.forEach((p) => p());
   },
   settings: Settings,
