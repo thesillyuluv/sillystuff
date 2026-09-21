27a3f6b6959aa92d771211418b7ff24a90b17e36
Silly <jdhdhelp@gmail.com>
2026-09-21 12:43:28 -0400

    Add Settings component for Discord Editor

---
+++ plugins/discordeditor/src/Settings.tsx
@@ -0,0 +1,23 @@
+import { React, ReactNative } from "@vendetta/metro/common";
+import { useProxy } from "@vendetta/storage";
+import { storage } from "@vendetta/plugin";
+import { Forms } from "@vendetta/ui/components";
+
+const { FormSection, FormInput, FormText } = Forms;
+const { ScrollView } = ReactNative;
+
+export default function Settings() {
+  useProxy(storage);
+
+  return (
+    <ScrollView>
+      <FormSection title="Discord Editor" titleStyleType="no_border">
+        <FormInput title="Name" placeholder="Discord" value={storage.name} onChange={(v: string) => (storage.name = v)} />
+        <FormInput title="Avatar" placeholder="https://example.com/avatar.png" value={storage.avatar} onChange={(v: string) => (storage.avatar = v)} />
+        <FormText style={{ margin: 16, opacity: 0.6 }}>
+          Leave a field empty to keep Discord's default name or avatar. You should reload after updating.
+        </FormText>
+      </FormSection>
+    </ScrollView>
+  );
+}
